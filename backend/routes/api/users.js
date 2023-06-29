0a8b958d8dee1561084a0a0ccd3937cf6fd560b1
Kyle Seeley <kyleseeley@Kyles-MacBook-Air-2.local>
2023-06-29 17:31:56 -0600

    signup a user

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,4 +1,36 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
+
+const { setTokenCookie, requireAuth } = require("../../utils/auth");
+const { User } = require("../../db/models");
+
 const router = express.Router();
+
+// Sign up
+router.post("/", async (req, res) => {
+  const { firstName, lastName, username, password, email } = req.body;
+  const hashedPassword = bcrypt.hashSync(password);
+  const user = await User.create({
+    firstName,
+    lastName,
+    username,
+    hashedPassword,
+    email,
+  });
+
+  const safeUser = {
+    id: user.id,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+    username: user.username,
+  };
+
+  await setTokenCookie(res, safeUser);
+
+  return res.json({
+    user: safeUser,
+  });
+});
 
 module.exports = router;
