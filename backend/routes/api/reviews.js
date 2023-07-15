7fa6a6cb2510158f37b25de0d1dcf42a113ff3dd
Kyle Seeley <kyleseeley@kyles-air-2.mynetworksettings.com>
2023-07-15 09:35:48 -0700

    added posting a review image

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -61,4 +61,39 @@ router.get("/current", requireAuth, async (req, res) => {
   res.json(response);
 });
 
+router.post("/:reviewId/images", requireAuth, async (req, res) => {
+  const reviewId = req.params.reviewId;
+  const userId = req.user.id;
+  const { url } = req.body;
+
+  // Check if the review exists and belongs to the current user
+  const review = await Review.findOne({
+    where: { id: reviewId, userId },
+  });
+  if (!review) {
+    return res.status(404).json({ message: "Review couldn't be found" });
+  }
+
+  // Check the number of existing images for the review
+  const existingImages = await ReviewImage.count({ where: { reviewId } });
+  if (existingImages > 10) {
+    return res.status(403).json({
+      message: "Maximum number of images for this resource was reached",
+    });
+  }
+
+  // Create the new image for the review
+  try {
+    const newImage = await ReviewImage.create({
+      reviewId,
+      url,
+    });
+
+    res.json({ id: newImage.id, url: newImage.url });
+  } catch (error) {
+    console.error("Error adding image to review:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
 module.exports = router;
