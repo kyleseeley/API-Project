f719157210252e256654748b302990375de325e3
Kyle Seeley <kyleseeley@Kyles-MacBook-Air-2.local>
2023-08-19 12:22:00 -0600

    done with phase 2 of authenticate me

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,58 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { Route, Switch } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Route, Switch, Redirect } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormPage";
 import SignupFormPage from "./components/SignupFormPage";
 import * as sessionActions from "./store/session";
 
 function App() {
   const dispatch = useDispatch();
+  const sessionUser = useSelector((state) => state.session.user);
   const [isLoaded, setIsLoaded] = useState(false);
+  const [userRestored, setUserRestored] = useState(false);
+
   useEffect(() => {
-    console.log("Effect triggered");
-    dispatch(sessionActions.restoreUser()).then(() => {
+    console.log("useEffect triggered is App.js");
+    if (!userRestored && sessionUser) {
+      dispatch(sessionActions.restoreUser()).then(() => {
+        setIsLoaded(true);
+        setUserRestored(true);
+        console.log("setLoaded if in App.js");
+      });
+    } else {
       setIsLoaded(true);
-    });
-  }, [dispatch, setIsLoaded]);
+      console.log("setLoaded else in App.js");
+    }
+  }, [dispatch, sessionUser, userRestored]);
 
   if (!isLoaded) {
-    return (
-      <Switch>
-        <Route path="/login">
-          <LoginFormPage />
-        </Route>
-        <Route path="/signup">
-          <SignupFormPage />
-        </Route>
-      </Switch>
-    );
+    return null;
   }
 
   return (
-    isLoaded && (
-      <Switch>
-        <Route path="/login">
-          <LoginFormPage />
-        </Route>
-        <Route path="/signup">
-          <SignupFormPage />
-        </Route>
-      </Switch>
-    )
+    <Switch>
+      <Route path="/login">
+        {sessionUser ? <Redirect to="/" /> : <LoginFormPage />}
+      </Route>
+      <Route path="/signup">
+        {sessionUser ? <Redirect to="/" /> : <SignupFormPage />}
+      </Route>
+      {/* Other routes */}
+    </Switch>
   );
+
+  // return (
+  //   isLoaded && (
+  //     <Switch>
+  //       <Route path="/login">
+  //         <LoginFormPage />
+  //       </Route>
+  //       <Route path="/signup">
+  //         <SignupFormPage />
+  //       </Route>
+  //     </Switch>
+  //   )
+  // );
 }
 
 export default App;
