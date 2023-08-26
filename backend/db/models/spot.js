f521743ce2bde47ed715447075aca792d68f6356
Kyle Seeley <kyleseeley@Kyles-MacBook-Air-2.local>
2023-08-26 11:15:32 -0600

    changed string to text in spots model

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -74,7 +74,7 @@ module.exports = (sequelize, DataTypes) => {
         },
       },
       description: {
-        type: DataTypes.STRING,
+        type: DataTypes.TEXT,
         allowNull: false,
       },
       price: {
