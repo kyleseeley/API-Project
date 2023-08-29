34c789c586ae4afd619e42ea052b0c66c2d59ce6
Kyle Seeley <kyleseeley@Kyles-MacBook-Air-2.local>
2023-08-28 19:40:02 -0600

    finished first section of create a new spot

--- frontend/src/components/CreateNewSpot/index.js
+++ frontend/src/components/CreateNewSpot/index.js
@@ -40,11 +40,81 @@ const CreateNewSpot = () => {
 
   return (
     <div className="create-spot-container">
-      <h2>Create a New Spot</h2>
+      <h2 className="main-header">Create a New Spot</h2>
       <form onSubmit={handleSubmit} className="create-spot-form">
-        {/* Input fields for each form field */}
-        {/* Remember to use the appropriate onChange handlers */}
-        {/* Submit button */}
+        <div className="located-header">
+          <h3>Where's your place located?</h3>
+          <p>
+            Guests will only get your exact address once they booked a
+            reservation.
+          </p>
+          <div className="located-input-group">
+            <label className="form-input-label">Country</label>
+            <input
+              className="form-input"
+              type="text"
+              placeholder="Country"
+              value={country}
+              onChange={(e) => setCountry(e.target.value)}
+              required
+            />
+            <label className="form-input-label">Street Address</label>
+            <input
+              className="form-input"
+              type="text"
+              placeholder="Address"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+              required
+            />
+            <div className="input-row">
+              <div className="input-column">
+                <label className="form-input-label">City</label>
+                <input
+                  className="form-input comma-after"
+                  type="text"
+                  placeholder="City"
+                  value={city}
+                  onChange={(e) => setCity(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="input-column">
+                <label className="form-input-label">State</label>
+                <input
+                  className="form-input"
+                  type="text"
+                  placeholder="State"
+                  value={state}
+                  onChange={(e) => setState(e.target.value)}
+                  required
+                />
+              </div>
+            </div>
+            <div className="input-row">
+              <div className="input-column">
+                <label className="form-input-label">Latitude</label>
+                <input
+                  className="form-input comma-after"
+                  type="text"
+                  placeholder="Latitude"
+                  value={lat}
+                  onChange={(e) => setLat(e.target.value)}
+                />
+              </div>
+              <div className="input-column">
+                <label className="form-input-label">Longitude</label>
+                <input
+                  className="form-input"
+                  type="text"
+                  placeholder="Longitude"
+                  value={lng}
+                  onChange={(e) => setLng(e.target.value)}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </form>
     </div>
   );
